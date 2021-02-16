a12ef5f1b0ddd2266745dbfe2b84aacc157a5d7f
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2021-02-16 15:40:05 +0700

    Create HomeController, AboutController, ArticleController

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    function index(){
+        echo "Dito Cahya Pratama - 1941720030";
+    }
+}
